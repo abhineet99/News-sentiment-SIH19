23089db1a80f812e60a249f89fdbb35bac09eff2
Abhineet Pandey <36227377+abhineet99@users.noreply.github.com>
2019-02-16 01:01:26 +0530

    Update toi.py

--- scrapper/toi.py
+++ scrapper/toi.py
@@ -1,8 +1,10 @@
-import os
+#this is a scrapper of Times of India news website
 import requests
 from bs4 import BeautifulSoup as bsp
-page = requests.get('https://timesofindia.indiatimes.com/topic/indian-railways') 
+page = requests.get('https://timesofindia.indiatimes.com/topic/indian-railways') #the toi railways topic link
 data = page.text
 soup= bsp(data,'html.parser')
 results = soup.findAll("span", {"class" : "title"})
-print (results)
+#print (results)
+for tag in results:
+	print(tag.string) #headlines will be printed, working on date and time now
