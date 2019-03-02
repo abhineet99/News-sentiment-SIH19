98ea721657061bed1c7e33a7adc6130f2fff0328
Nilansh Rajput <rajputnilansh@gmail.com>
2019-03-03 04:58:52 +0530

    Add files via upload

--- sentiment/interface.py
+++ sentiment/interface.py
@@ -18,7 +18,7 @@ def convert_text_to_index_array(text):
 		if word in dictionary:
 			wordIndices.append(dictionary[word])
 	return wordIndices
-labels = ['happy','not_happy']
+labels = [0,1]
 with open('dictionary.json', 'r') as dictionary_file:
 	dictionary = json.load(dictionary_file)
 
@@ -32,30 +32,36 @@ model.load_weights('model.h5')
 
 #taking inputs 
 inputMatrix = pd.read_csv(SOURCE)
+
 clen = len ( inputMatrix['Headline'] )
 tokenizer = Tokenizer(num_words=10000) 
-
+inputMatrix['SentimentScore'] = np.nan
+ 
+inputMatrix['Uploaded'] = np.nan
+inputMatrix['Category'] = np.nan
 y_pred_pos = []
-y_pred_neg = []
+sentiment_ = []
 print (clen)
 for i in range(clen):
 	try:
 		headLine = inputMatrix['Headline'][i]
 		testArr = convert_text_to_index_array(headLine)
-		if not inputMatrix['Positive'][i]:
-			y_pred_pos.append(inputMatrix['Positive'][i])
-			y_pred_neg.append(inputMatrix['Negative'][i])
+		if (np.nan== inputMatrix['SentimentScore'][i]):
+			y_pred_pos.append(inputMatrix['SentimentScore'][i])
+			sentiment_.append(inputMatrix['Sentiment'][i])
 			continue
 		headLineInp = tokenizer.sequences_to_matrix([testArr], mode='binary')
 		pred = model.predict(headLineInp)
 		y_pred_pos.append(pred[0][0])
-		y_pred_neg.append(pred[0][1])
+		sentiment_.append(np.argmax(pred))
+		# y_pred_neg.append(pred[0][1])
 	except AttributeError:
 		break
+    
 
 print(len(y_pred_pos))
-raw_data = {'Source': inputMatrix['Source'] ,'Headline': inputMatrix['Headline'], 'Date' : inputMatrix['Date'],  'Positive':  y_pred_pos , 'Negative' : y_pred_neg }
-df = pd.DataFrame(raw_data, columns = ['Source', 'Headline' , 'Date' , 'Positive' , 'Negative'])
+raw_data = {'Source': inputMatrix['Source'] , 'Headline': inputMatrix['Headline'], 'SourceURL':inputMatrix['SourceURL'], 'Date' : inputMatrix['Date'], 'Description' : inputMatrix['Details'], 'ImageURL' : inputMatrix['ImageURL'] , 'Sentiment': sentiment_, 'SentimentScore':y_pred_pos , 'Uploaded' : inputMatrix['Uploaded']  }
+df = pd.DataFrame(raw_data, columns = ['Source', 'Headline' , 'SourceURL', 'Date', 'Details', 'ImageURL', 'Sentiment', 'SentimentScore', 'Uploaded'  ])
 df.to_csv(DESTINATION, sep = ',' , index= False)
 
 	
