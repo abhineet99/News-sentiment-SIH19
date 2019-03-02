4772754834c9dc77b01e2abdd42fd5b8d45a4d1d
i-am-g2 <2017csb1083@iitrpr.ac.in>
2019-03-02 07:54:26 +0530

    Added requirements.txt

--- AdPrincingSIH/AdPrice/API/resources.py
+++
@@ -1,7 +0,0 @@
-from tastypie.resources import ModelResource
-from AdPriceAPI.models import NEWSPAPER_DATA
-
-class NEWSPAPER_DATA_RES (ModelResource):
-	class Meta:
-		queryset = NEWSPAPER_DATA.objects.all()
-		allowed_methods = ['get']
--- AdPrincingSIH/AdPrice/AdPriceAPI/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import NEWSPAPER_DATA
-
-# Register your models here.
-admin.site.register(NEWSPAPER_DATA)
